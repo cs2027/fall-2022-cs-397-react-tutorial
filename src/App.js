458cfb897bb5e389e628cdd096c2e7f29ab4e4a8
Christopher Song <chris4728.song2001@gmail.com>
2022-09-27 14:33:40 -0500

    Fetch data

--- src/App.js
+++ src/App.js
@@ -1,42 +1,27 @@
 import Banner from './components/Banner';
 import CourseList from './components/CourseList';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useJsonQuery } from './utilities/fetch';
 import './App.css';
 
-const schedule = {
-  "title": "CS Courses for 2018-2019",
-  "courses": {
-    "F101" : {
-      "term": "Fall",
-      "number": "101",
-      "meets" : "MWF 11:00-11:50",
-      "title" : "Computer Science: Concepts, Philosophy, and Connections"
-    },
-    "F110" : {
-      "term": "Fall",
-      "number": "110",
-      "meets" : "MWF 10:00-10:50",
-      "title" : "Intro Programming for non-majors"
-    },
-    "S313" : {
-      "term": "Spring",
-      "number": "313",
-      "meets" : "TuTh 15:30-16:50",
-      "title" : "Tangible Interaction Design and Learning"
-    },
-    "S314" : {
-      "term": "Spring",
-      "number": "314",
-      "meets" : "TuTh 9:30-10:50",
-      "title" : "Tech & Human Interaction"
-    }
-  }
+const queryClient = new QueryClient();
+
+const Main = () => {
+  const [scheduleData, isLoading, error] = useJsonQuery("https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php");
+
+  if (error) return <h1>Error retrieving course data: {`${error}`}</h1>;
+  if (isLoading) return <h1>Retrieving course data...</h1>;
+
+  return <div className="app-body">
+      <Banner title={scheduleData.title} />
+      <CourseList courses={scheduleData.courses} />
+    </div>;
 };
 
 const App = () => (
-  <div className='app-body'>
-    <Banner title={schedule.title} />
-    <CourseList courses={schedule.courses} />
-  </div>
+  <QueryClientProvider client={queryClient}>
+    <Main />
+  </QueryClientProvider>
 );
 
 export default App;
--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -1,15 +1,9 @@
 import './Course.css';
 
-// "F110" : {
-//   "term": "Fall",
-//   "number": "110",
-//   "meets" : "MWF 10:00-10:50",
-//   "title" : "Intro Programming for non-majors"
-// },
 const Course = ({course}) => (
   <div className="course">
     <div className="course-top">
-      <div class="course-header">{course.term} CS {course.number}</div>
+      <div className="course-header">{course.term} CS {course.number}</div>
       <div>{course.title}</div>
     </div>
     <hr />
