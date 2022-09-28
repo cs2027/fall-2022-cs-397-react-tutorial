11cff13644c28a548f46fd76a99b38bd7fb3d5d6
Christopher Song <chris4728.song2001@gmail.com>
2022-09-27 21:51:03 -0500

    Implement selecting courses

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -1,14 +1,17 @@
 import './Course.css';
 
-const Course = ({course}) => (
-  <div className="course">
-    <div className="course-top">
-      <div className="course-header">{course.term} CS {course.number}</div>
-      <div>{course.title}</div>
+const Course = ({course, isSelected, toggleSelect}) => {
+
+  return (
+    <div className={isSelected ? "course course-selected" : "course"} onClick={() => toggleSelect(course)}>
+      <div className="course-top">
+        <div className="course-header">{course.term} CS {course.number}</div>
+        <div>{course.title}</div>
+      </div>
+      <hr />
+      <div>{course.meets}</div>
     </div>
-    <hr />
-    <div>{course.meets}</div>
-  </div>
-);
+  );
+};
 
 export default Course;
--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -1,10 +1,30 @@
+import { useState } from 'react';
 import Course from './Course';
 import './CourseList.css';
 
-const CourseList = ({courses}) => (
-  <div className="course-list">
-    { courses.map(([id, course]) => <Course key={id} course={course} />) }
-  </div>
-);
+const CourseList = ({courses}) => {
+  const [selectedCourses, setSelectedCourses] = useState([]);
+
+  const toggleSelect = (course) => setSelectedCourses(
+    selectedCourses.includes(course)
+    ? selectedCourses.filter(selectedCourse => selectedCourse !== course)
+    : [...selectedCourses, course]
+  );
+
+  return (
+    <div className="course-list">
+      {
+        courses.map(([id, course]) => (
+          <Course
+            key={id}
+            course={course}
+            isSelected={selectedCourses.includes(course)}
+            toggleSelect={toggleSelect}
+          />
+        ))
+      }
+    </div>
+  );
+};
 
 export default CourseList;
