3054f2e74d1f7d3ebe0cb5d8f100233d6e2af222
Christopher Song <chris4728.song2001@gmail.com>
2022-10-04 14:03:13 -0500

    Add form validation

--- src/components/CourseEditPage.jsx
+++ src/components/CourseEditPage.jsx
@@ -1,19 +1,38 @@
 import { useParams, Link } from "react-router-dom";
 import InputGroup from "./InputGroup";
+import { useFormData } from '../utilities/useFormData';
 import "./CourseEditPage.css";
+
+const validateUserData = (key, val) => {
+  switch (key) {
+    case 'title':
+      return /(^\w\w)/.test(val) ? '' : 'must be least two characters';
+    case 'meets':
+      return /^$/.test(val) || /^(\w)+\s\d{1,2}:\d{1,2}-\d{1,2}:\d{1,2}$/.test(val)
+            ? ''
+            : 'must be either empty string OR must contain days and start-end, e.g. MWF 12:00-13:20';
+    default: return '';
+  }
+};
 
 const CourseEditPage = ({courses}) => {
   const { courseId } = useParams();
   const courseData = Object.entries(courses).filter(course => course[0] === courseId)[0][1];
+  const courseDataFormSubset = (({ title, meets }) => ({ title, meets }))(courseData);
+
+  const [state, change] = useFormData(validateUserData, courseDataFormSubset);
+  const submit = (evt) => {
+    evt.preventDefault();
+  };
 
   return (
     <div className="edit-course-page">
       <h1 className="edit-course-header">Edit Course: {courseId}</h1>
-      <form onSubmit={() => {}}>
-        <InputGroup label="Course Number" defaultValue={courseData.number} />
-        <InputGroup label="Course Title" defaultValue={courseData.title} />
-        <InputGroup label="Meeting Times" defaultValue={courseData.meets} />
+      <form onSubmit={submit} noValidate>
+        <InputGroup keyName="title" label="Course Title" defaultValue={courseData.title} state={state} change={change}/>
+        <InputGroup keyName="meets" label="Meeting Times" defaultValue={courseData.meets} state={state} change={change}/>
         <div className="button-group">
+          <button type="submit" className="button submit-button">Submit</button>
           <Link to="/">
             <input className="button cancel-button" defaultValue="Cancel" />
           </Link>
--- src/components/InputGroup.jsx
+++ src/components/InputGroup.jsx
@@ -1,9 +1,17 @@
 import "./InputGroup.css";
 
-const InputGroup = ({label, defaultValue}) => (
+const InputGroup = ({keyName, label, state, change}) => (
   <div className="input-group">
     <label className="input-label">{label}</label>
-    <input className="input-bar" defaultValue={defaultValue} placeholder={label}/>
+    <input
+      className={`input-bar ${state.errors?.[keyName] ? 'invalid-input-bar' : ''}`}
+      id={keyName}
+      name={keyName}
+      defaultValue={state.values?.[keyName]}
+      onChange={change}
+      placeholder={label}
+    />
+    <div className="invalid-input-msg">{state.errors?.[keyName]}</div>
   </div>
 );
 
